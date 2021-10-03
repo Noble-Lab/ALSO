f892233cf5c64516f0bca775e6b3515b5b318240
Kris Alavattam <kalavattam@gmail.com>
2021-10-02 17:23:06 -0700

    Updating scripts by CX.

--- bin/generate_gene_body_file.R
+++ bin/generate_gene_body_file.R
@@ -1,25 +1,47 @@
+#!/usr/bin/env Rscript
+
+#  Chengxiang (CX) Qiu
+#  Adapted, edited by KA
+
 library(rtracklayer)
 library(argparse)
 
-# Avoid scientific notation
-options(scipen=999)
+#  Avoid scientific notation
+options(scipen = 999)
 options(stringsAsFactors = FALSE)
 
-parser = argparse::ArgumentParser(description='Makes a BED file of TSSs provided a GENCODE GTF file.')
-parser$add_argument('input_file', help='GENCODE GTF file.')
-parser$add_argument('output_file', help='Output gene body BED file.')
-args = parser$parse_args()
+parser <- argparse::ArgumentParser(description = 'Makes a .bed file of gene bodies provided a GENCODE/Ensembl .gtf file.')
+parser$add_argument('input_file', help = 'GENCODE/Ensembl .gtf file.')
+parser$add_argument('output_file', help = 'Output gene body .bed file.')
+args <- parser$parse_args()
 
 get_gene_body <- function(gene_ann) {
-  gene_ann = subset(gene_ann, type == "gene")
-  gene_ann <- gene_ann[!duplicated(gene_ann$gene_id),]
+    gene_ann <- subset(gene_ann, type == "gene")
+    gene_ann <- gene_ann[!duplicated(gene_ann$gene_id), ]
 
-  # Output BED format
-  gene_ann[,"start"] <- gene_ann[,"start"] - 1
+    #  Output .bed format
+    gene_ann[, "start"] <- gene_ann[, "start"] - 1
 
-  return(gene_ann)
+    return(gene_ann)
 }
 
-gene_bodies = get_gene_body(rtracklayer::readGFF(args$input_file))
-gene_bodies$score = '.'
-write.table(gene_bodies[, c('seqid', 'start', 'end', 'gene_id', 'score', 'strand', 'gene_name', 'gene_type')], quote=F, row.names=F, col.names=F, sep='\t', file=args$output_file)
+gene_bodies <- get_gene_body(rtracklayer::readGFF(args$input_file))
+gene_bodies$score <- '.'
+write.table(
+    gene_bodies[,
+    c(
+        'seqid',
+        'start',
+        'end',
+        'gene_id',
+        'score',
+        'strand',
+        'gene_name',
+        'gene_type'
+    )],
+    quote = FALSE,
+    row.names = FALSE,
+    col.names = FALSE,
+    sep = '\t',
+    file = args$output_file
+)
--- bin/generate_tss_file.R
+++ bin/generate_tss_file.R
@@ -1,37 +1,59 @@
+#!/usr/bin/env Rscript
+
+#  Chengxiang (CX) Qiu
+#  Adapted, edited by KA
+
 library(rtracklayer)
 library(argparse)
 
-# Avoid scientific notation
-options(scipen=999)
+#  Avoid scientific notation
+options(scipen = 999)
 options(stringsAsFactors = FALSE)
 
-parser = argparse::ArgumentParser(description='Makes a BED file of TSSs provided a GENCODE GTF file.')
-parser$add_argument('input_file', help='GENCODE GTF file.')
-parser$add_argument('output_file', help='Output TSS BED file.')
-args = parser$parse_args()
+parser <- argparse::ArgumentParser(description = 'Makes a TSS .bed file when provided a GENCODE/Ensembl .gtf file.')
+parser$add_argument('input_file', help = 'GENCODE/Ensembl .gtf file.')
+parser$add_argument('output_file', help = 'Output TSS .bed file.')
+args <- parser$parse_args()
 
-get_tss <- function(gene_ann) {
-  gene_ann = subset(gene_ann, type == "transcript")
-  pos <- subset(gene_ann, strand == "+")
-  neg <- subset(gene_ann, strand == "-")
-  pos <- pos[order(pos[,"start"]),]
-  neg <- neg[order(neg[,"start"], decreasing = TRUE),]
-  pos <- pos[!duplicated(pos$transcript_id),]
-  neg <- neg[!duplicated(neg$transcript_id),]
+get_tss <- function(gene_annotation) {
+    gene_annotation <- subset(gene_annotation, type == "transcript")
+    pos <- subset(gene_annotation, strand == "+")
+    neg <- subset(gene_annotation, strand == "-")
+    pos <- pos[order(pos[, "start"]), ]
+    neg <- neg[order(neg[, "start"], decreasing = TRUE), ]
+    pos <- pos[!duplicated(pos$transcript_id), ]
+    neg <- neg[!duplicated(neg$transcript_id), ]
 
-  # Output BED format
-  pos[,"end"] <- pos[,"start"]
-  pos[,"start"] <- pos["start"] - 1
+    #  Output .bed format
+    pos[, "end"] <- pos[, "start"]
+    pos[, "start"] <- pos["start"] - 1
 
-  neg[,"start"] <- neg[,"end"] - 1
-  neg[,"end"] <- neg[,"start"] + 1
-  return(rbind(pos, neg))
+    neg[, "start"] <- neg[, "end"] - 1
+    neg[, "end"] <- neg[, "start"] + 1
+    return(rbind(pos, neg))
 }
 
-tss = get_tss(rtracklayer::readGFF(args$input_file))
+tss <- get_tss(rtracklayer::readGFF(args$input_file))
 
 if ('transcript_support_level' %in% colnames(tss)) {
-  tss = subset(tss, is.na(transcript_support_level) | transcript_support_level >= 2)
+    tss <- subset(tss, is.na(transcript_support_level) | transcript_support_level >= 2)
 }
-tss$score = '.'
-write.table(tss[, c('seqid', 'start', 'end', 'gene_id', 'score', 'strand', 'gene_name', 'transcript_id', 'transcript_type')], quote=F, row.names=F, col.names=F, sep='\t', file=args$output_file)
+tss$score <- '.'
+write.table(
+    tss[, c(
+        'seqid',
+        'start',
+        'end',
+        'gene_id',
+        'score',
+        'strand',
+        'gene_name',
+        'transcript_id',
+        'transcript_type'
+    )],
+    quote = FALSE,
+    row.names = FALSE,
+    col.names = FALSE,
+    sep = '\t',
+    file = args$output_file
+)
